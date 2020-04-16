ecf122d4051725dd89b6d84eb1045046e420a32b
Brian Wu <poenbwu@gmail.com>
2020-04-15 18:30:17 -0700

    create basic graph without structure using soc data

--- src/soc_preprocessor.py
+++ src/soc_preprocessor.py
@@ -4,14 +4,24 @@ information via the Spreadsheet from US Bureau of Labor Statistics.
 '''
 
 import os
+import openpyxl
+import networkx
 
 
 def main():
-    data_file = os.path.join(
-        os.path.abspath('./data'),
-        'soc_2018_definitions.xlsx')
-    with open(data_file, mode="r") as fp:
-        return
+    filepath = os.path.join(
+        os.path.abspath('./data'), 'soc_2018_definitions.xlsx')
+    graph = networkx.Graph()
+    # Keeping this structure to add other types data to the graph easier
+    graph = load_soc_data(filepath, graph)
+
+
+def load_soc_data(filepath, graph):
+    workbook = openpyxl.load_workbook(filepath)
+    worksheet = workbook.worksheets[0]
+    for row in worksheet.values:
+        graph.add_node(row)
+    workbook.close()
 
 
 if __name__ == '__main__':
