0875e566bcbe917017532085a726e0fa74b5f5b6
Brian Wu <poenbwu@gmail.com>
2020-04-15 20:44:16 -0700

    load basic data into graph

--- src/soc_preprocessor.py
+++ src/soc_preprocessor.py
@@ -6,6 +6,7 @@ information via the Spreadsheet from US Bureau of Labor Statistics.
 import os
 import openpyxl
 import networkx
+import pprint
 
 
 def main():
@@ -13,15 +14,17 @@ def main():
         os.path.abspath('./data'), 'soc_2018_definitions.xlsx')
     graph = networkx.Graph()
     # Keeping this structure to add other types data to the graph easier
-    graph = load_soc_data(filepath, graph)
+    graph = load_xlsx_data(filepath, graph)
 
 
-def load_soc_data(filepath, graph):
+def load_xlsx_data(filepath, graph):
     workbook = openpyxl.load_workbook(filepath)
     worksheet = workbook.worksheets[0]
     for row in worksheet.values:
-        graph.add_node(row)
+        if row[0] == 'Detailed':
+            graph.add_node(row[1], title=row[2])
     workbook.close()
+    return graph
 
 
 if __name__ == '__main__':
