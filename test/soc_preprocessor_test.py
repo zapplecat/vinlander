f5e511c17c953289cc8ea5bbe9a89f0647682655
Brian Wu <poenbwu@gmail.com>
2020-04-19 02:00:30 -0700

    add dummy test to be filled out

---
+++ test/soc_preprocessor_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestLoadXlsxData(unittest.TestCase):
+
+    def test_1(self):
+        pass
+
+
+class TestSetCodeMapping(unittest.TestCase):
+
+    def test_1(self):
+        pass
