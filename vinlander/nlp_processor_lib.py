5ad020e2e5661c78c048b0ac0c5de253333ea33d
Brian Wu <poenbwu@gmail.com>
2020-06-07 01:50:09 -0700

    spot cleaning nlp lib

--- vinlander/nlp_processor_lib.py
+++ vinlander/nlp_processor_lib.py
@@ -2,13 +2,35 @@ import networkx
 import os
 import spacy
 
+# TOOD: Swap this out for larger model
 nlp = spacy.load("en_core_web_md")
 
-def write_graph_to_pickle(graph):
+
+def write_graph_to_pickle(graph, filename, nlp_attrib='nlp_vector'):
+    # Accept a filter func
     for node in graph.nodes:
-        if node in code_map['detailed']:
-            graph.nodes[node]['nlp_vector'] = nlp(
+        if graph.nodes[node]['soc_type'] == 'detailed':
+            graph.nodes[node][nlp_attrib] = nlp(
                 graph.nodes[node]['job_description'])
     pickle_filepath = os.path.join(
-        os.path.abspath('./data'), 'nlp_md_graph')
+        os.path.abspath('./data'), filename)
     networkx.write_gpickle(graph, pickle_filepath)
+
+
+def get_node_similarity(
+        node1, node2, comparison_attrib='job_description'):
+    """Gets the description similarilty between leaf nodes.
+    """
+    doc1 = nlp(node1[comparison_attrib])
+    doc2 = nlp(node2[comparison_attrib])
+    return doc1.similarity(doc2)
+
+
+def get_node_similarity_from_graph(
+        graph, node1, node2, nlp_attrib='nlp_vector'):
+    if not graph.has_node(node1):
+        raise RuntimeError("node1 {} is not in graph".format(node1))
+    elif not graph.has_node(node2):
+        raise RuntimeError("node2 {} is not in graph".format(node2))
+    return graph.nodes[node1][nlp_attrib].similarity(
+        graph.nodes[node2][nlp_attrib])
